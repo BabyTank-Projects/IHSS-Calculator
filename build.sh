2b0d68d5aa5281fc2d9bd9d4d1e984ec395066fc
Baby Tank <thriveinterror@gmail.com>
2026-02-14 00:46:54 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,11 @@ pyinstaller --onefile \
     --windowed \
     --name "IHSS_Calendar_Generator" \
     --icon=NONE \
+    --hidden-import=openpyxl \
+    --hidden-import=openpyxl.styles \
+    --hidden-import=openpyxl.utils \
+    --hidden-import=PIL \
+    --hidden-import=PIL.ImageGrab \
     --add-data "requirements.txt:." \
     ihsscalculator_enhanced.py
 
